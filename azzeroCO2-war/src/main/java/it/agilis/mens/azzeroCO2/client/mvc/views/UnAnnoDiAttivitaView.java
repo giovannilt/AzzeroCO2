3304ecd678d7035124eb73229ec1daf0e83a30ec
Giovanni La Torre <giovannilt@gmail.com>
2011-07-02 23:47:47 +0200

    Anno di attivita'

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/views/UnAnnoDiAttivitaView.java
@@ -0,0 +1,39 @@
+package it.agilis.mens.azzeroCO2.client.mvc.views;
+
+import com.extjs.gxt.ui.client.event.EventType;
+import com.extjs.gxt.ui.client.mvc.AppEvent;
+import com.extjs.gxt.ui.client.mvc.Controller;
+import com.extjs.gxt.ui.client.mvc.Dispatcher;
+import com.extjs.gxt.ui.client.mvc.View;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import it.agilis.mens.azzeroCO2.client.mvc.events.AzzeroCO2Events;
+import it.agilis.mens.azzeroCO2.client.mvc.events.CentralEvents;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: giovannilt
+ * Date: 7/2/11
+ * Time: 11:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnAnnoDiAttivitaView extends View {
+     private ContentPanel unAnnoDiAttivita = new ContentPanel();
+    public UnAnnoDiAttivitaView(Controller controller) {
+        super(controller);
+    }
+
+
+    @Override
+    protected void handleEvent(AppEvent event) {
+        EventType eventType = event.getType();
+        if (eventType.equals(AzzeroCO2Events.Init)) {
+            onInit(event);
+        }
+    }
+
+
+    private void onInit(AppEvent event) {
+        Dispatcher.forwardEvent(new AppEvent(CentralEvents.UnAnnoDiAttivitaPanelReady,
+                unAnnoDiAttivita));
+    }
+}
