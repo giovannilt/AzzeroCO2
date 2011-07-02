1a68ac236bfe48aab26d41640d73e69ae5157d08
Giovanni La Torre <giovannilt@gmail.com>
2011-07-02 23:15:03 +0200

    O

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/shared/model/ProgettiDiCompensazione.java
@@ -0,0 +1,14 @@
+package it.agilis.mens.azzeroCO2.shared.model;
+
+import com.extjs.gxt.ui.client.data.BaseModelData;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: giovannilt
+ * Date: 7/2/11
+ * Time: 6:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProgettiDiCompensazione extends BaseModelData{
+
+}
