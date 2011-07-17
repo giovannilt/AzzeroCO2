f962310dec841682281dfdce3cd252a00daaa7a0
Serena Di Maida <serdyserdy@yahoo.it>
2011-07-17 18:39:56 +0200

    Amministrazione/Ordini e Coupon

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/shared/model/Ordine.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.shared.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 17/07/11
+ * Time: 16.06
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ordine {
+}
