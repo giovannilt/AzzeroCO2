c1c8abd78253388d065732a82a0d79b21374860d
Giovanni La Torre <giovannilt@gmail.com>
2011-08-16 22:51:27 +0200

    ok

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/shared/Utils.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.shared;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: giovannilt
+ * Date: 8/16/11
+ * Time: 10:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Utils {
+}
