eb105215fae632f60e8d2591a631e1a725b1d980
Giovanni La Torre <giovannilt@gmail.com>
2011-08-16 23:13:20 +0200

    c

--- azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/shared/Utils.java
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/shared/Utils.java
@@ -7,5 +7,29 @@ package it.agilis.mens.azzeroCO2.shared;
  * Time: 10:49 PM
  * To change this template use File | Settings | File Templates.
  */
+
+import it.agilis.mens.azzeroCO2.shared.model.amministrazione.Ordine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
+
+
+    public static List<Ordine> getOrdini(List<it.agilis.mens.azzeroCO2.core.entity.Ordine> ordini){
+        List<it.agilis.mens.azzeroCO2.shared.model.amministrazione.Ordine> ordiniModel= new ArrayList<Ordine>();
+
+                           // TODO FOR
+
+        for(it.agilis.mens.azzeroCO2.core.entity.Ordine ordine: ordini){
+           it.agilis.mens.azzeroCO2.shared.model.amministrazione.Ordine o= new it.agilis.mens.azzeroCO2.shared.model.amministrazione.Ordine();
+            o.setCliente(ordine.getUtente().getUserName());
+
+            ordiniModel.add(o);
+        }
+
+
+        return ordiniModel;
+
+    }
 }
