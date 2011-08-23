8e469fb34390203f13528028d2e361d50b0cf031
Serena Di Maida <serena.dimaida@gmail.com>
2011-08-23 17:22:24 +0200

    RegisterDialog

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/events/RegisterEvents.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.mvc.events;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 23/08/11
+ * Time: 16:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegisterEvents {
+}
---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/views/RegisterView.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.mvc.views;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 23/08/11
+ * Time: 16:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegisterView {
+}
