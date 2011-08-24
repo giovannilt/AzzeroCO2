1b113f020aa11afd97cf27ee204fb4c161e13107
Giovanni La Torre <giovannilt@gmail.com>
2011-08-25 00:18:22 +0200

    NIsbanada

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/shared/model/registrazione/RegistrazioneModel.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.shared.model.registrazione;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: giovannilt
+ * Date: 8/24/11
+ * Time: 9:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegistrazioneModel {
+}
