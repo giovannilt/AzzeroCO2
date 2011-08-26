a158b0470c5b1efe0618a68a3bcc5947701f2d04
Serena Di Maida <serena.dimaida@gmail.com>
2011-08-27 00:24:40 +0200

    Fa abbastanza schifo ma intanto c'è... domani lo sistemo

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/forms/amministrazione/UserInfo.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.forms.amministrazione;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 26/08/11
+ * Time: 22:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserInfo {
+}
