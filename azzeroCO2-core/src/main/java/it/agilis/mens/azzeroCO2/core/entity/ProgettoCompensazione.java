c0af8841d8c2188c9721546ead564b231adcf069
Serena Di Maida <serena.dimaida@gmail.com>
2011-08-28 15:43:12 +0200

    un po di entity

---
+++ azzeroCO2-core/src/main/java/it/agilis/mens/azzeroCO2/core/entity/ProgettoCompensazione.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.core.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 28/08/11
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProgettoCompensazione {
+}
