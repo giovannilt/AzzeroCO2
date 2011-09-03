4d4f6f59ae8d8ad1cceaef6fc2e8830138211bbf
Serena Di Maida <serena.dimaida@gmail.com>
2011-09-03 16:52:14 +0200

    sql tipo carta

---
+++ azzeroCO2-core/src/sql/TipoDiCarta.sql
@@ -0,0 +1,45 @@
+INSERT
+INTO
+    TipoDiCarta
+    (id,parametro,nome)
+    VALUES
+    (1,'PUBRIS','Carta riciclata sbiancata') ;
+
+
+    INSERT
+INTO
+    TipoDiCarta
+    (id,parametro,nome)
+    VALUES
+    (2,'PUBRIC','Carta riciclata') ;
+
+    INSERT
+INTO
+    TipoDiCarta
+    (id,parametro,nome)
+    VALUES
+    (3,'PUBNOP','Carta non patinata') ;
+
+
+    INSERT
+INTO
+    TipoDiCarta
+    (id,parametro,nome)
+    VALUES
+    (4,'PUBPAT','Carta patinata') ;
+
+
+
+
+INTO
+    TipoDiCarta
+    (id,parametro,nome)
+    VALUES
+    (5,'PUBGIO','Carta da giornale') ;
+
+
+INTO
+    TipoDiCarta
+    (id,parametro,nome)
+    VALUES
+    (6,PUBRIV,'Carta da rivista') ;
