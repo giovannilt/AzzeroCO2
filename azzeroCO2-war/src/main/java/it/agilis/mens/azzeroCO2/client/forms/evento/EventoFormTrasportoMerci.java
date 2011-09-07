dceb990fb9ef2f8e7f56837f39af3df85a4ea5b6
Serena Di Maida <serena.dimaida@gmail.com>
2011-09-07 12:56:50 +0200

    trasp merci

--- azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/forms/evento/EventoFormTrasportoMerci.java
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/forms/evento/EventoFormTrasportoMerci.java
@@ -70,180 +70,245 @@ public class EventoFormTrasportoMerci extends LayoutContainer {
         c2.add(new LabelField("Inserisci il numero di tonnellate di merce per chilometraggio a mezzo di trasporto. Il trasporto viene calcolato su medie di chilometraggio"), flex);
 
         panel.add(c2);
-        LayoutContainer piu50 = new LayoutContainer();
-        HBoxLayout layoutRigaPiu50 = new HBoxLayout();
-        layoutRigaPiu50.setPadding(new Padding(10));
-        piu50.setLayout(layoutRigaPiu50);
-        piu50.add(new LabelField("Distanza percorsa >50 km"), flex);
-        panel.add(piu50);
-        LayoutContainer piu50input = new LayoutContainer();
-        HBoxLayout layoutPiu50input = new HBoxLayout();
-        layoutPiu50input.setPadding(new Padding(10));
-        layoutPiu50input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu50input.setLayout(layoutPiu50input);
+        LayoutContainer km30 = new LayoutContainer();
+        HBoxLayout layoutRigaKm30 = new HBoxLayout();
+        layoutRigaKm30.setPadding(new Padding(10));
+        km30.setLayout(layoutRigaKm30);
+        km30.add(new LabelField("Distanza percorsa: provinciale"), flex);
+        panel.add(km30);
+        LayoutContainer km30input = new LayoutContainer();
+        HBoxLayout layoutKm30input = new HBoxLayout();
+        layoutKm30input.setPadding(new Padding(10));
+        layoutKm30input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km30input.setLayout(layoutKm30input);
 
-        Image bus = new Image(AzzeroCO2Resources.INSTANCE.bus());
-        bus.setAltText("Bus");
-        NumberField busPiu50 = new NumberField();
-        busPiu50.setName("busPiu50");
-        busPiu50.setWidth(60);
+        Image furgone = new Image(AzzeroCO2Resources.INSTANCE.furgone());
+        furgone.setAltText("Furgone");
+        NumberField furgoneKm30 = new NumberField();
+        furgoneKm30.setName("furgoneKm30");
+        furgoneKm30.setWidth(60);
 
-        Image automobile = new Image(AzzeroCO2Resources.INSTANCE.automobile());
-        bus.setAltText("Auto");
-        NumberField autoPiu50 = new NumberField();
-        autoPiu50.setName("autoPiu50");
-        autoPiu50.setWidth(60);
-
-        Image treno = new Image(AzzeroCO2Resources.INSTANCE.treno());
-        bus.setAltText("Treno");
-        NumberField trenoPiu50 = new NumberField();
-        trenoPiu50.setName("trenoPiu50");
-        trenoPiu50.setWidth(60);
-
-        piu50input.add(bus);
-        piu50input.add(busPiu50, flex);
-        piu50input.add(automobile);
-        piu50input.add(autoPiu50, flex);
-        piu50input.add(treno);
-        piu50input.add(trenoPiu50, flex);
-
-        layoutRigaPiu50.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        panel.add(piu50input, new FormData("100%"));
-
-        LayoutContainer piu100 = new LayoutContainer();
-        HBoxLayout layoutRigaPiu100 = new HBoxLayout();
-        layoutRigaPiu100.setPadding(new Padding(10));
-        layoutRigaPiu100.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu100.setLayout(layoutRigaPiu100);
-
-        piu100.add(new LabelField("Distanza percorsa >100 km"), flex);
-        panel.add(piu100);
-
-        LayoutContainer piu100input = new LayoutContainer();
-        HBoxLayout layoutPiu100input = new HBoxLayout();
-        layoutPiu100input.setPadding(new Padding(10));
-        layoutPiu100input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu100input.setLayout(layoutPiu100input);
+        Image tir = new Image(AzzeroCO2Resources.INSTANCE.tir());
+        furgone.setAltText("Tir");
+        NumberField tirKm30 = new NumberField();
+        tirKm30.setName("tirKm30");
+        tirKm30.setWidth(60);
 
 
-        Image bus100 = new Image(AzzeroCO2Resources.INSTANCE.bus());
-        bus100.setAltText("Bus");
-        NumberField busPiu100 = new NumberField();
-        busPiu100.setName("busPiu100");
-        busPiu100.setWidth(60);
+        km30input.add(furgone);
+        km30input.add(furgoneKm30, flex);
+        km30input.add(tir);
+        km30input.add(tirKm30, flex);
 
-        Image automobile100 = new Image(AzzeroCO2Resources.INSTANCE.automobile());
-        automobile100.setAltText("Auto");
-        NumberField autoPiu100 = new NumberField();
-        autoPiu100.setName("autoPiu100");
-        autoPiu100.setWidth(60);
+        layoutRigaKm30.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        panel.add(km30input, new FormData("100%"));
 
-        Image treno100 = new Image(AzzeroCO2Resources.INSTANCE.treno());
-        treno100.setAltText("Treno");
-        NumberField trenoPiu100 = new NumberField();
-        trenoPiu100.setName("trenoPiu100");
-        trenoPiu100.setWidth(60);
+        LayoutContainer km150 = new LayoutContainer();
+        HBoxLayout layoutRigaKm150 = new HBoxLayout();
+        layoutRigaKm150.setPadding(new Padding(10));
+        layoutRigaKm150.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km150.setLayout(layoutRigaKm150);
 
-        piu100input.add(bus100);
-        piu100input.add(busPiu100, flex);
-        piu100input.add(automobile100);
-        piu100input.add(autoPiu100, flex);
-        piu100input.add(treno100);
-        piu100input.add(trenoPiu100, flex);
-        panel.add(piu100input, new FormData("100%"));
+        km150.add(new LabelField("Distanza percorsa: regionale"), flex);
+        panel.add(km150);
 
-        LayoutContainer piu250 = new LayoutContainer();
-        HBoxLayout layoutRigaPiu250 = new HBoxLayout();
-        layoutRigaPiu250.setPadding(new Padding(10));
-        layoutRigaPiu250.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu250.setLayout(layoutRigaPiu250);
-        piu250.add(new LabelField("Distanza percorsa >250 km"), flex);
-        panel.add(piu250);
-
-        LayoutContainer piu250input = new LayoutContainer();
-        HBoxLayout layoutPiu250input = new HBoxLayout();
-        layoutPiu250input.setPadding(new Padding(10));
-        layoutPiu250input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu250input.setLayout(layoutPiu250input);
-
-        Image bus250 = new Image(AzzeroCO2Resources.INSTANCE.bus());
-        bus250.setAltText("Bus");
-        NumberField busPiu250 = new NumberField();
-        busPiu250.setName("busPiu250");
-        busPiu250.setWidth(60);
+        LayoutContainer km150input = new LayoutContainer();
+        HBoxLayout layoutKm150input = new HBoxLayout();
+        layoutKm150input.setPadding(new Padding(10));
+        layoutKm150input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km150input.setLayout(layoutKm150input);
 
 
-        Image automobile250 = new Image(AzzeroCO2Resources.INSTANCE.automobile());
-        automobile250.setAltText("Auto");
-        NumberField autoPiu250 = new NumberField();
-        autoPiu250.setName("autoPiu250");
-        autoPiu250.setWidth(60);
+        Image furgone150 = new Image(AzzeroCO2Resources.INSTANCE.furgone());
+        furgone150.setAltText("Bus");
+        NumberField furgoneKm150 = new NumberField();
+        furgoneKm150.setName("furgoneKm150");
+        furgoneKm150.setWidth(60);
 
-        Image treno250 = new Image(AzzeroCO2Resources.INSTANCE.treno());
-        treno250.setAltText("Treno");
-        NumberField trenoPiu250 = new NumberField();
-        trenoPiu250.setName("trenoPiu250");
-        trenoPiu250.setWidth(60);
+        Image tir150 = new Image(AzzeroCO2Resources.INSTANCE.tir());
+        tir150.setAltText("Tir");
+        NumberField tirKm150 = new NumberField();
+        tirKm150.setName("tirKm150");
+        tirKm150.setWidth(60);
 
-        Image aereo250 = new Image(AzzeroCO2Resources.INSTANCE.aereo());
-        aereo250.setAltText("Aereo");
-        NumberField aereoPiu250 = new NumberField();
-        aereoPiu250.setName("aereoPiu250");
-        aereoPiu250.setWidth(60);
+        Image treno150 = new Image(AzzeroCO2Resources.INSTANCE.treno());
+        treno150.setAltText("Treno");
+        NumberField trenoKm150 = new NumberField();
+        trenoKm150.setName("trenoKm150");
+        trenoKm150.setWidth(60);
 
-        piu250input.add(bus250);
-        piu250input.add(busPiu250, flex);
-        piu250input.add(automobile250);
-        piu250input.add(autoPiu250, flex);
-        piu250input.add(treno250);
-        piu250input.add(trenoPiu250, flex);
-        piu250input.add(aereo250);
-        piu250input.add(aereoPiu250, flex);
-        panel.add(piu250input, new FormData("100%"));
+        km150input.add(furgone150);
+        km150input.add(furgoneKm150, flex);
+        km150input.add(tir150);
+        km150input.add(tirKm150, flex);
+        km150input.add(treno150);
+        km150input.add(trenoKm150, flex);
+        panel.add(km150input, new FormData("100%"));
 
-        LayoutContainer piu500 = new LayoutContainer();
-        HBoxLayout layoutRigaPiu500 = new HBoxLayout();
-        layoutRigaPiu500.setPadding(new Padding(10));
-        layoutRigaPiu500.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu500.setLayout(layoutRigaPiu500);
+        LayoutContainer km500 = new LayoutContainer();
+        HBoxLayout layoutRigaKm500 = new HBoxLayout();
+        layoutRigaKm500.setPadding(new Padding(10));
+        layoutRigaKm500.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km500.setLayout(layoutRigaKm500);
+        km500.add(new LabelField("Distanza percorsa: nazionale"), flex);
+        panel.add(km500);
 
-        piu500.add(new LabelField("Distanza percorsa >500 km"), flex);
-        panel.add(piu500);
-        LayoutContainer piu500input = new LayoutContainer();
-        HBoxLayout layoutPiu500input = new HBoxLayout();
-        layoutPiu500input.setPadding(new Padding(10));
-        layoutPiu500input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
-        piu500input.setLayout(layoutPiu500input);
+        LayoutContainer km500input = new LayoutContainer();
+        HBoxLayout layoutKm500input = new HBoxLayout();
+        layoutKm500input.setPadding(new Padding(10));
+        layoutKm500input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km500input.setLayout(layoutKm500input);
 
-        Image bus500 = new Image(AzzeroCO2Resources.INSTANCE.bus());
-        bus500.setAltText("Bus");
-        NumberField busPiu500 = new NumberField();
-        busPiu500.setWidth(60);
-        Image automobile500 = new Image(AzzeroCO2Resources.INSTANCE.automobile());
-        automobile500.setAltText("Auto");
-        NumberField autoPiu500 = new NumberField();
-        autoPiu500.setWidth(60);
+        Image furgone500 = new Image(AzzeroCO2Resources.INSTANCE.furgone());
+        furgone500.setAltText("Bus");
+        NumberField furgoneKm500 = new NumberField();
+        furgoneKm500.setName("furgoneKm500");
+        furgoneKm500.setWidth(60);
+
+
+        Image tir500 = new Image(AzzeroCO2Resources.INSTANCE.tir());
+        tir500.setAltText("Auto");
+        NumberField tirKm500 = new NumberField();
+        tirKm500.setName("tirKm500");
+        tirKm500.setWidth(60);
 
         Image treno500 = new Image(AzzeroCO2Resources.INSTANCE.treno());
         treno500.setAltText("Treno");
-        NumberField trenoPiu500 = new NumberField();
-        trenoPiu500.setWidth(60);
+        NumberField trenoKm500 = new NumberField();
+        trenoKm500.setName("trenoKm500");
+        trenoKm500.setWidth(60);
 
-        Image aereo500 = new Image(AzzeroCO2Resources.INSTANCE.aereo());
-        aereo500.setAltText("Aereo");
+        Image nave500 = new Image(AzzeroCO2Resources.INSTANCE.nave());
+        nave500.setAltText("Nave");
+        NumberField naveKm500 = new NumberField();
+        naveKm500.setName("naveKm500");
+        naveKm500.setWidth(60);
 
-        NumberField aereoPiu500 = new NumberField();
-        aereoPiu500.setWidth(60);
-        piu500input.add(bus500);
-        piu500input.add(busPiu500, flex);
-        piu500input.add(automobile500);
-        piu500input.add(autoPiu500, flex);
-        piu500input.add(treno500);
-        piu500input.add(trenoPiu500, flex);
-        piu500input.add(aereo500);
-        piu500input.add(aereoPiu500, flex);
-        panel.add(piu500input, new FormData("100%"));
+        km500input.add(furgone500);
+        km500input.add(furgoneKm500, flex);
+        km500input.add(tir500);
+        km500input.add(tirKm500, flex);
+        km500input.add(treno500);
+        km500input.add(trenoKm500, flex);
+        km500input.add(nave500);
+        km500input.add(naveKm500, flex);
+        panel.add(km500input, new FormData("100%"));
 
+        LayoutContainer km1500 = new LayoutContainer();
+        HBoxLayout layoutRigaKm15001500 = new HBoxLayout();
+        layoutRigaKm15001500.setPadding(new Padding(10));
+        layoutRigaKm15001500.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km1500.setLayout(layoutRigaKm15001500);
+
+        km1500.add(new LabelField("Distanza percorsa: europea"), flex);
+        panel.add(km1500);
+        LayoutContainer km1500input = new LayoutContainer();
+        HBoxLayout layoutKm15001500input = new HBoxLayout();
+        layoutKm15001500input.setPadding(new Padding(10));
+        layoutKm15001500input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km1500input.setLayout(layoutKm15001500input);
+
+        Image furgone1500 = new Image(AzzeroCO2Resources.INSTANCE.furgone());
+        furgone1500.setAltText("Furgone");
+        NumberField furgoneKm1500 = new NumberField();
+        furgoneKm1500.setWidth(60);
+
+        Image tir1500 = new Image(AzzeroCO2Resources.INSTANCE.tir());
+        tir1500.setAltText("Tir");
+        NumberField tirKm1500 = new NumberField();
+        tirKm1500.setWidth(60);
+
+        Image treno1500 = new Image(AzzeroCO2Resources.INSTANCE.treno());
+        treno1500.setAltText("Treno");
+        NumberField trenoKm15001500 = new NumberField();
+        trenoKm15001500.setWidth(60);
+
+        Image nave1500 = new Image(AzzeroCO2Resources.INSTANCE.nave());
+        nave1500.setAltText("Nave");
+        NumberField naveKm15001500 = new NumberField();
+        naveKm15001500.setWidth(60);
+
+
+
+        Image aereo1500 = new Image(AzzeroCO2Resources.INSTANCE.aereo());
+        aereo1500.setAltText("Aereo");
+        NumberField aereoKm15001500 = new NumberField();
+        aereoKm15001500.setWidth(60);
+
+        km1500input.add(furgone1500);
+        km1500input.add(furgoneKm1500, flex);
+        km1500input.add(tir1500);
+        km1500input.add(tirKm1500, flex);
+        km1500input.add(treno1500);
+        km1500input.add(trenoKm15001500, flex);
+        km1500input.add(nave1500);
+        km1500input.add(naveKm15001500, flex);
+
+        km1500input.add(aereo1500);
+        km1500input.add(aereoKm15001500, flex);
+        panel.add(km1500input, new FormData("100%"));
+
+        
+        
+        
+        
+        
+        LayoutContainer km9000 = new LayoutContainer();
+        HBoxLayout layoutRigaKm90009000 = new HBoxLayout();
+        layoutRigaKm90009000.setPadding(new Padding(10));
+        layoutRigaKm90009000.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km9000.setLayout(layoutRigaKm90009000);
+
+        km9000.add(new LabelField("Distanza percorsa: Extraeuropea"), flex);
+        panel.add(km9000);
+        LayoutContainer km9000input = new LayoutContainer();
+        HBoxLayout layoutKm90009000input = new HBoxLayout();
+        layoutKm90009000input.setPadding(new Padding(10));
+        layoutKm90009000input.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.MIDDLE);
+        km9000input.setLayout(layoutKm90009000input);
+
+        Image furgone9000 = new Image(AzzeroCO2Resources.INSTANCE.furgone());
+        furgone9000.setAltText("Furgone");
+        NumberField furgoneKm9000 = new NumberField();
+        furgoneKm9000.setWidth(60);
+
+        Image tir9000 = new Image(AzzeroCO2Resources.INSTANCE.tir());
+        tir9000.setAltText("Tir");
+        NumberField tirKm9000 = new NumberField();
+        tirKm9000.setWidth(60);
+
+        Image treno9000 = new Image(AzzeroCO2Resources.INSTANCE.treno());
+        treno9000.setAltText("Treno");
+        NumberField trenoKm90009000 = new NumberField();
+        trenoKm90009000.setWidth(60);
+
+        Image nave9000 = new Image(AzzeroCO2Resources.INSTANCE.nave());
+        nave9000.setAltText("Nave");
+        NumberField naveKm90009000 = new NumberField();
+        naveKm90009000.setWidth(60);
+
+
+
+        Image aereo9000 = new Image(AzzeroCO2Resources.INSTANCE.aereo());
+        aereo9000.setAltText("Aereo");
+        NumberField aereoKm90009000 = new NumberField();
+        aereoKm90009000.setWidth(60);
+
+        km9000input.add(furgone9000);
+        km9000input.add(furgoneKm9000, flex);
+        km9000input.add(tir9000);
+        km9000input.add(tirKm9000, flex);
+        km9000input.add(treno9000);
+        km9000input.add(trenoKm90009000, flex);
+        km9000input.add(nave9000);
+        km9000input.add(naveKm90009000, flex);
+
+        km9000input.add(aereo9000);
+        km9000input.add(aereoKm90009000, flex);
+        panel.add(km9000input, new FormData("100%"));
+        
+        
+        
         return panel;
     }
 
