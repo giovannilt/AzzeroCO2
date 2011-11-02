a218542bc212f621687dcbf5fdc04454f0a05ed0
Giovanni La Torre <giovannilt@gmail.com>
2011-11-02 13:48:02 +0100

    Loggin save Save / Login Rebase (cherry-picking from commit fa9fc58744d7e1081fa90d6e94ec2d388bf048ce) Salva (cherry-picking from commit 8f9101d1b1a384b020800afd887f995d14ee7ef3) Salva (cherry-picking from commit 8f9101d1b1a384b020800afd887f995d14ee7ef3) (cherry-picking from commit 6f291593e7bf917e485f9bd617d886e164adb254) Rebase (cherry-picking from commit fa9fc58744d7e1081fa90d6e94ec2d388bf048ce) Salva (cherry-picking from commit 8f9101d1b1a384b020800afd887f995d14ee7ef3) Salva (cherry-picking from commit 8f9101d1b1a384b020800afd887f995d14ee7ef3) Rebase (cherry-picking from commit fa9fc58744d7e1081fa90d6e94ec2d388bf048ce) Rebase (cherry-picking from commit fa9fc58744d7e1081fa90d6e94ec2d388bf048ce) Salva (cherry-picking from commit 8f9101d1b1a384b020800afd887f995d14ee7ef3) Salva (cherry-picking from commit 8f9101d1b1a384b020800afd887f995d14ee7ef3) (cherry-picking from commit 6f291593e7bf917e485f9bd617d886e164adb254)

--- azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/controllers/EventoController.java
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/controllers/EventoController.java
@@ -98,30 +98,32 @@ public class EventoController extends BaseController {
 
         } else if (event.getType().equals(AzzeroCO2Events.LoggedIn)) {
             setUserInfoModel((UserInfoModel) event.getData());
+    
+
         } else if (event.getType().equals(EventoEvents.Save)) {
-            if(getUserInfoModel().getProfilo()== Profile.Guest.ordinal()){
-
-                return;
-            }
-            final DettaglioVTO riepilogo = AzzerroCO2UtilsClientHelper.getDettaglioVTO(eventoView.getRiepilogo());
-            if (riepilogo.getNome() == null || riepilogo.getNome().length() == 0) {
-                Info.display("Warning", "Nome Evento Mancante");
-            } else {
-                AsyncCallback<DettaglioVTO> dettaglio = new AsyncCallback<DettaglioVTO>() {
-                    public void onFailure(Throwable caught) {
-                        Info.display("Error", "Errore impossibile connettersi al server " + caught);
-                    }
-
-                    @Override
-                    public void onSuccess(DettaglioVTO result) {
-                        if (result != null) {
-                            eventoView.setDettaglioModel(AzzerroCO2UtilsClientHelper.getDettaglioModel(result));
-                            Info.display("Info", "Evento " + riepilogo.getNome() + " salvato con successo.");
+            if (getUserInfoModel().getProfilo() == Profile.Guest.ordinal()) {
+               Dispatcher.forwardEvent(LoginEvents.ShowForm);
+            }else{
+                final DettaglioVTO riepilogo = AzzerroCO2UtilsClientHelper.getDettaglioVTO(eventoView.getRiepilogo());
+                if (riepilogo.getNome() == null || riepilogo.getNome().length() == 0) {
+                    Info.display("Warning", "Nome Evento Mancante");
+                } else {
+                    AsyncCallback<DettaglioVTO> dettaglio = new AsyncCallback<DettaglioVTO>() {
+                        public void onFailure(Throwable caught) {
+                            Info.display("Error", "Errore impossibile connettersi al server " + caught);
                         }
-                    }
-                };
 
-                getHustonService().saveOrdine(riepilogo, dettaglio);
+                        @Override
+                        public void onSuccess(DettaglioVTO result) {
+                            if (result != null) {
+                                eventoView.setDettaglioModel(AzzerroCO2UtilsClientHelper.getDettaglioModel(result));
+                                Info.display("Info", "Evento " + riepilogo.getNome() + " salvato con successo.");
+                            }
+                        }
+                    };
+
+                    getHustonService().saveOrdine(riepilogo, dettaglio);
+                }
             }
         } else {
             forwardToView(eventoView, event);
