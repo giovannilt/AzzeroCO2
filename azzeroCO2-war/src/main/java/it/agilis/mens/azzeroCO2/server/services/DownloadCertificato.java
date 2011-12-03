e19ed6578e45d10db650d3201256be02d33e71b4
Giovanni La Torre <giovannilt@gmail.com>
2011-12-03 21:44:22 +0100

    Download Certificati

--- azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/server/services/DownloadCertificato.java
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/server/services/DownloadCertificato.java
@@ -1,14 +1,18 @@
 package it.agilis.mens.azzeroCO2.server.services;
 
-import it.agilis.mens.azzeroCO2.core.register.impl.AzzeroCO2Register;
+import it.agilis.mens.azzeroCO2.server.PropertiesManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,16 +22,28 @@ import java.io.IOException;
  * To change this template use File | Settings | File Templates.
  */
 public class DownloadCertificato extends HttpServlet {
-    @Autowired
-    @Qualifier("azzeroCO2Register")
-    private AzzeroCO2Register azzeroCO2Register;
+    private BufferedInputStream bis;
 
-    public AzzeroCO2Register getAzzeroCO2Register() {
-        return azzeroCO2Register;
+   @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        WebApplicationContext ctx = WebApplicationContextUtils
+                .getRequiredWebApplicationContext(config.getServletContext());
+        AutowireCapableBeanFactory beanFactory = ctx
+                .getAutowireCapableBeanFactory();
+        beanFactory.autowireBean(this);
     }
 
-    public void setAzzeroCO2Register(AzzeroCO2Register azzeroCO2Register) {
-        this.azzeroCO2Register = azzeroCO2Register;
+    @Autowired
+    @Qualifier("propertiesManager")
+    private PropertiesManager propertiesManager;
+
+    public PropertiesManager getPropertiesManager() {
+        return propertiesManager;
+    }
+
+    public void setPropertiesManager(PropertiesManager propertiesManager) {
+        this.propertiesManager = propertiesManager;
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -38,6 +54,21 @@ public class DownloadCertificato extends HttpServlet {
         response.setContentType("application/pdf");
         response.setHeader("Content-disposition", "attachment; filename=Certificato.pdf");
 
-        String certificato = request.getParameter("Certificato");
+        String certificato = request.getParameter("certificato");
+
+        String path = propertiesManager.getCertificatiFolder()+certificato;
+
+        bis = new BufferedInputStream(new FileInputStream(new File(path)));
+
+        int read=0;
+        byte[] bytes = new byte[1024];
+        OutputStream os = response.getOutputStream();
+        while ((read = bis.read(bytes)) != -1) {
+            os.write(bytes, 0, read);
+        }
+
+        os.flush();
+        os.close();
+
     }
 }
