bf09e6c2158909a58884a5cfca5a47876966d92a
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-07 00:16:33 +0100

    Anno

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/annoAttivita/AnnoWest.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.components.annoAttivita;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 06/12/11
+ * Time: 23:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnnoWest {
+}
