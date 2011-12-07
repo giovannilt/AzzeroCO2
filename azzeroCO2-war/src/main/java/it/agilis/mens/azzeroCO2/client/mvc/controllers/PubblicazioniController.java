30cea1f1e738a42feb7902f8db89289760eecaff
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-07 23:23:13 +0100

    pubblicazione

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/controllers/PubblicazioniController.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.mvc.controllers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 07/12/11
+ * Time: 22:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class PubblicazioniController {
+}
---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/events/PubblicazioniEvents.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.mvc.events;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 07/12/11
+ * Time: 22:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class PubblicazioniEvents {
+}
