6dc409ecff0df6ccc69f65ff0a63a87d081aefb4
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-07 18:24:02 +0100

    ?

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/forms/annoAttivita/AnnoFormDettaglio.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.forms.annoAttivita;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 07/12/11
+ * Time: 10:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnnoFormDettaglio {
+}
