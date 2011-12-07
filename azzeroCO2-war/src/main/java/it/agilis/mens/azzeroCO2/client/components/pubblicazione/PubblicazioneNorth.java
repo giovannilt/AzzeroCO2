36da9166e5561855cf2c6d94084186333f2c0f06
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-07 22:31:11 +0100

    pubbl1

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/pubblicazione/PubblicazioneNorth.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.components.pubblicazione;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 07/12/11
+ * Time: 18:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class PubblicazioneNorth {
+}
---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/mvc/views/UnaPubblicazioneView.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.mvc.views;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 07/12/11
+ * Time: 22:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnaPubblicazioneView {
+}
