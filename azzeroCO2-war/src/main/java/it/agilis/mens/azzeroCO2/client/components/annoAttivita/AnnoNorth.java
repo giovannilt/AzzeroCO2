b0977c51b0fedb406fd63042b25e56b524b0b548
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-08 22:31:55 +0100

    north

--- azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/annoAttivita/AnnoNorth.java
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/annoAttivita/AnnoNorth.java
@@ -32,7 +32,7 @@ public class AnnoNorth extends LayoutContainer  {
         layout.setHBoxLayoutAlign(HBoxLayout.HBoxLayoutAlign.TOP);
         c.setLayout(layout);
 
-        c.add(new Html("<p style=\"padding:1px;font-family:tahoma,arial,verdana,sans-serif;color:#000000;\">Compensa un Evento </p>"));
+        c.add(new Html("<p style=\"padding:1px;font-family:tahoma,arial,verdana,sans-serif;color:#000000;\">Compensa un anno di attività </p>"));
 
         HBoxLayoutData flex = new HBoxLayoutData(new Margins(0, 5, 0, 0));
         flex.setFlex(1);
