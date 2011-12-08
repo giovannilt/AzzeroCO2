7ecbcda9fdd718a247ba9a69ddde4758da4b8ee3
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-08 21:20:57 +0100

    fissarii

--- azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/pubblicazione/PubblicazioneWest.java
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/pubblicazione/PubblicazioneWest.java
@@ -40,7 +40,7 @@ import java.util.List;
 public class PubblicazioneWest extends LayoutContainer {
     private Grid<RiepilogoModel> grid;
     private ListStore<RiepilogoModel> store = new ListStore<RiepilogoModel>();
-    private Text title = new Text("Evento");
+    private Text title = new Text("Pubblicazioni");
     private final String oggettoDiDefault = "Non hai ancora inserito </br> nessuna attivita'";
     private final String riepilogoString = "Hai Terminato il Calcolo! </br>" +
             "Se vuoi modifica i dati inseriti,</br>" +
