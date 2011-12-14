b0dd55339b0f09ea26567359f15cecc971430f20
Serena Di Maida <serena.dimaida@gmail.com>
2011-12-14 18:24:36 +0100

    dialog

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/components/EnergiaInfoDialog.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.components;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serenadimaida
+ * Date: 14/12/11
+ * Time: 15:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnergiaInfoDialog {
+}
