d8fae99a8c9401523770e99b7b05e54ae5ae9b33
seryserdy <serena.dimaida@gmail.com>
2012-02-01 22:16:50 +0100

    sitoweb si è moltiplicato

---
+++ azzeroCO2-war/src/main/java/it/agilis/mens/azzeroCO2/client/forms/FormSitoWebSolo.java
@@ -0,0 +1,11 @@
+package it.agilis.mens.azzeroCO2.client.forms;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: serdyserdy
+ * Date: 01/02/12
+ * Time: 21:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class FormSitoWebSolo {
+}
